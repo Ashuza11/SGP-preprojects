ab2ec97dc06b4474e78e5b22671a9013bc08376e
M ash <ashuza1411@gmail.com>
2022-06-09 00:34:15 +0200

    Add update user profile pic field

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User  
@@ -41,6 +42,7 @@ class UpdateAccountForm(FlaskForm):
                     validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                     validators=[DataRequired(), Email()])
+    picture =  FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     # User WTF For validating form
